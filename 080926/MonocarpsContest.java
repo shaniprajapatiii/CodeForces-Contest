82e1426952670fdf14b08fae1337be7071407c44
Shani Prajapati <shaniprajapati630@gmail.com>
2026-09-08 20:20:31 +0530

    08/09/2026

---
+++ 080926/MonocarpsContest.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class MonocarpsContest {
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int t = sc.nextInt();
+      while(t-- > 0) {
+         int n = sc.nextInt();
+         int[] a = new int[n];
+
+         int zero = 0;
+         for(int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+            if(a[i] == 0) zero++;
+         }
+
+         if(zero < 2) {
+            System.err.println(-1);
+         }
+         else if(a[0] == 0 && a[n - 1] == 0) {
+            System.out.println(0);
+         }
+         else if(a[0] == 0 || a[n - 1] == 0) {
+            System.out.println(1);
+         }
+         else {
+            System.out.println(2);
+         }
+      }
+      sc.close();
+   }
+}
